3721431e6a4b3a6eecb61f6fdc9d4037b70243bb
Ansel <83976339+ansel64@users.noreply.github.com>
2024-02-20 13:36:08 +0300

    Update button.py

--- button.py
+++ button.py
@@ -1,7 +1,7 @@
 from pyray import *
 
 class Button(object):
-    '''A square button with position x and y, size (side length), and color.'''
+    '''A square button with position x and y, size (side length), and its current color.'''
     def __init__(self, x: int, y:int , size: int, color):
         self.x = x
         self.y = y
@@ -10,5 +10,5 @@ class Button(object):
     
 
     def drawButton(self):
-        '''Draw the button to the screen. '''
+        '''Draws the button to the screen at its appropriate position.'''
         draw_rectangle(self.x+1, self.y+1, self.size-1, self.size-1, self.color) # Draw the square one pixel smaller to give the 'grid' effect.
